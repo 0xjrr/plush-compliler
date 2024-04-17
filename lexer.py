7e035a360d2b5eb528724d10c63b39b76c10f1ef
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-04-17 19:19:54 +0100

    add TYPE

--- lexer.py
+++ lexer.py
@@ -7,7 +7,8 @@ tokens = [
     'EQUALS', 'NE', 'GE', 'GT', 'LE', 'LT',
     'AND', 'OR', 'NOT',
     'VAL', 'VAR', 'FUNCTION', 'IF', 'ELSE', 'WHILE',
-    'TRUE', 'FALSE', 'SEMICOLON', 'COLON', 'COMMA', 'ASSIGN'
+    'TRUE', 'FALSE', 'SEMICOLON', 'COLON', 'COMMA', 'ASSIGN',
+    'TYPE'
 ]
 
 t_PLUS = r'\+'
@@ -42,7 +43,13 @@ reserved = {
     'else': 'ELSE',
     'while': 'WHILE',
     'true': 'TRUE',
-    'false': 'FALSE'
+    'false': 'FALSE',
+    'int': 'TYPE',
+    'float': 'TYPE',
+    'string': 'TYPE',
+    'double': 'TYPE',
+    'void': 'TYPE',
+    'bool': 'TYPE'
 }
 
 # Identifiers
@@ -91,8 +98,8 @@ def t_error(t):
 lexer = lex.lex()
 
 
-# Test it out
-data = '''
+# Test
+test_small = '''
 val x : int := 1;
 var y : float := 1.2;
 # This is a comment
@@ -104,7 +111,64 @@ if (x > y) {
 '''
 
 # Give the lexer some input
-lexer.input(data)
+lexer.input(test_small)
+
+print("\n --- Small test ---\n")
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break  # No more input
+    print(tok)
+
+test_large = '''
+# Define a function to calculate the square of a number
+function square(val num : int) : int {
+    square := num * num;
+}
+
+# Define a function to check if a number is positive, negative, or zero
+function checkNumber(val x : int) : string {
+    if (x > 0) {
+        checkNumber := "positive";
+    } else if (x < 0) {
+        checkNumber := "negative";
+    } else {
+        checkNumber := "zero";
+    }
+}
+
+# Main program starts here
+val pi : double := 3.14159;
+var radius : double := 2.5;
+val area : double := pi * square(radius); # Calculate the area of a circle
+
+# Using a loop to count down from 5
+var counter : int := 5;
+while (counter > 0) {
+    println("Counting down: " + counter);
+    counter := counter - 1;
+}
+
+# Checking if area is greater than a threshold
+if (area > 10) {
+    println("Large circle");
+} else {
+    println("Small circle");
+}
+
+# Checking number sign
+var number : int := -42;
+val sign : string := checkNumber(number);
+println("The number is " + sign);
+
+'''
+
+# Give the lexer some input
+lexer.input(test_large)
+
+print("\n --- Large test ---\n")
 
 # Tokenize
 while True:
