dbb35ccb16985cf538729970896753cac5ef0c04
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-04-17 20:39:03 +0100

    formated

--- lexer.py
+++ lexer.py
@@ -1,93 +1,128 @@
 import ply.lex as lex
 
 tokens = [
-    'NUMBER', 'FLOAT', 'STRING', 'IDENTIFIER',
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD',
-    'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE',
-    'EQUALS', 'NE', 'GE', 'GT', 'LE', 'LT',
-    'AND', 'OR', 'NOT',
-    'VAL', 'VAR', 'FUNCTION', 'IF', 'ELSE', 'WHILE',
-    'TRUE', 'FALSE', 'SEMICOLON', 'COLON', 'COMMA', 'ASSIGN',
-    'TYPE'
+    "NUMBER",
+    "FLOAT",
+    "STRING",
+    "IDENTIFIER",
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
+    "MOD",
+    "LPAREN",
+    "RPAREN",
+    "LBRACE",
+    "RBRACE",
+    "EQUALS",
+    "NE",
+    "GE",
+    "GT",
+    "LE",
+    "LT",
+    "AND",
+    "OR",
+    "NOT",
+    "VAL",
+    "VAR",
+    "FUNCTION",
+    "IF",
+    "ELSE",
+    "WHILE",
+    "TRUE",
+    "FALSE",
+    "SEMICOLON",
+    "COLON",
+    "COMMA",
+    "ASSIGN",
+    "TYPE",
 ]
 
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_MOD = r'%'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
-t_EQUALS = r'=='
-t_NE = r'!='
-t_GE = r'>='
-t_GT = r'>'
-t_LE = r'<='
-t_LT = r'<'
-t_AND = r'&&'
-t_OR = r'\|\|'
-t_NOT = r'!'
-t_SEMICOLON = r';'
-t_COLON = r':'
-t_COMMA = r','
-t_ASSIGN = r':='
+t_PLUS = r"\+"
+t_MINUS = r"-"
+t_TIMES = r"\*"
+t_DIVIDE = r"/"
+t_MOD = r"%"
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_LBRACE = r"\{"
+t_RBRACE = r"\}"
+t_EQUALS = r"=="
+t_NE = r"!="
+t_GE = r">="
+t_GT = r">"
+t_LE = r"<="
+t_LT = r"<"
+t_AND = r"&&"
+t_OR = r"\|\|"
+t_NOT = r"!"
+t_SEMICOLON = r";"
+t_COLON = r":"
+t_COMMA = r","
+t_ASSIGN = r":="
 
 # Keywords
 reserved = {
-    'val': 'VAL',
-    'var': 'VAR',
-    'function': 'FUNCTION',
-    'if': 'IF',
-    'else': 'ELSE',
-    'while': 'WHILE',
-    'true': 'TRUE',
-    'false': 'FALSE',
-    'int': 'TYPE',
-    'float': 'TYPE',
-    'string': 'TYPE',
-    'double': 'TYPE',
-    'void': 'TYPE',
-    'bool': 'TYPE'
+    "val": "VAL",
+    "var": "VAR",
+    "function": "FUNCTION",
+    "if": "IF",
+    "else": "ELSE",
+    "while": "WHILE",
+    "true": "TRUE",
+    "false": "FALSE",
+    "int": "TYPE",
+    "float": "TYPE",
+    "string": "TYPE",
+    "double": "TYPE",
+    "void": "TYPE",
+    "bool": "TYPE",
 }
+
 
 # Identifiers
 def t_IDENTIFIER(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'IDENTIFIER')  # Check for reserved words
+    r"[a-zA-Z_][a-zA-Z_0-9]*"
+    t.type = reserved.get(t.value, "IDENTIFIER")  # Check for reserved words
     return t
+
 
 # Float literals
 def t_FLOAT(t):
-    r'\d+\.\d*|\.\d+'
+    r"\d+\.\d*|\.\d+"
     t.value = float(t.value)
     return t
 
+
 # Number literals
 def t_NUMBER(t):
-    r'\d+'
+    r"\d+"
     t.value = int(t.value)
     return t
 
+
 # String literals
 def t_STRING(t):
-    r'\".*?\"'
+    r"\".*?\" "
     t.value = t.value[1:-1]  # Remove quotes
     return t
 
+
 # Comments
 def t_COMMENT(t):
-    r'\#.*'
+    r"\#.*"
     pass  # Token is discarded
 
+
 # Whitespace (spaces and tabs)
-t_ignore = ' \t'
+t_ignore = " \t"
+
 
 # Newlines
 def t_newline(t):
-    r'\n+'
+    r"\n+"
     t.lexer.lineno += len(t.value)
+
 
 # Error handling
 def t_error(t):
@@ -99,7 +134,7 @@ lexer = lex.lex()
 
 
 # Test
-test_small = '''
+test_small = """
 val x : int := 1;
 var y : float := 1.2;
 # This is a comment
@@ -108,7 +143,7 @@ if (x > y) {
 } else {
     val result : string := "y is greater or equal";
 }
-'''
+"""
 
 # Give the lexer some input
 lexer.input(test_small)
@@ -118,11 +153,11 @@ print("\n --- Small test ---\n")
 # Tokenize
 while True:
     tok = lexer.token()
-    if not tok: 
+    if not tok:
         break  # No more input
     print(tok)
 
-test_large = '''
+test_large = """
 # Define a function to calculate the square of a number
 function square(val num : int) : int {
     square := num * num;
@@ -163,7 +198,7 @@ var number : int := -42;
 val sign : string := checkNumber(number);
 println("The number is " + sign);
 
-'''
+"""
 
 # Give the lexer some input
 lexer.input(test_large)
@@ -173,6 +208,6 @@ print("\n --- Large test ---\n")
 # Tokenize
 while True:
     tok = lexer.token()
-    if not tok: 
+    if not tok:
         break  # No more input
     print(tok)
