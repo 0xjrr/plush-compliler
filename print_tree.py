b3aa1b5d4619193babec54ea25011ff799e5b557
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-05-06 01:33:40 +0100

    add pretty print method

--- print_tree.py
+++ print_tree.py
@@ -1,26 +1,22 @@
-def print_tree(node, indent=0):
-    if isinstance(node, tuple):
-        print('  ' * indent + node[0])
-        for item in node[1:]:
-            print_tree(item, indent + 1)
+import ast_nodes
+
+def pretty_print(node, indent=0):
+    """
+    Pretty prints the parsed AST node with proper indentation.
+    """
+    if isinstance(node, ast_nodes.Program):
+        print("Program(")
+        pretty_print(node.declarations, indent + 1)
+        print(")")
     elif isinstance(node, list):
         for item in node:
-            print_tree(item, indent)
-    elif isinstance(node, str):
-        print('  ' * indent + node)
-    elif isinstance(node, tuple) and len(node) == 5:
-        print('  ' * indent + f'{node[0]}: {node[2]} {node[3]} = {node[4]}')
-    elif isinstance(node, tuple) and len(node) == 3:
-        print('  ' * indent + f'{node[0]}: {node[1]} {node[2]}')
-    elif isinstance(node, tuple) and len(node) == 4:
-        print('  ' * indent + f'{node[0]}: {node[2]} {node[3]} {node[1]}')
-    elif isinstance(node, tuple) and len(node) == 2:
-        print('  ' * indent + f'{node[0]}: {node[1]}')
-    elif isinstance(node, tuple) and len(node) == 1:
-        print('  ' * indent + f'{node[0]}')
+            pretty_print(item, indent)
+    elif hasattr(node, "__dict__"):
+        node_name = type(node).__name__
+        print("  " * indent + node_name + "(")
+        for key, value in node.__dict__.items():
+            print("  " * (indent + 1) + f"{key} =", end=" ")
+            pretty_print(value, indent + 1)
+        print("  " * indent + ")")
     else:
-        print('  ' * indent + str(node))
-
-def display_tree(input_data):
-    for item in input_data:
-        print_tree(item)
+        print("  " * indent + repr(node))
