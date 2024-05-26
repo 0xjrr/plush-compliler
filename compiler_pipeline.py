9ecc6448fc064150e9eefbab03182bbfd2771b6b
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-05-26 15:58:38 +0100

    Refactor compiler_pipeline.py to read input from file and improve code organization

--- compiler_pipeline.py
+++ compiler_pipeline.py
@@ -5,22 +5,11 @@ import llvmir_compiler
 import os
 
 if __name__ == "__main__":
-    s = """
-    function main(): int {
-        var x : int := 120;
-        var y : int := 10;
-        x++;
-        y--;
-        ++x;
-        --y;
-        --x;
-        x += 1;
-        y -= 1;
-        x := x + y    + (x                                          *4);
-        return x + y;
-    }
-    """
+    with open("./example.pl", "r") as f:
+        s = f.read()
+    
     result = parser.parse(s)
+    print(result)
     semantics_checker = semantics_check.TypeChecker()
     semantics_checker.check_program(result)
     generator = llvmir_compiler.LLVMIRGenerator(result)
@@ -28,8 +17,6 @@ if __name__ == "__main__":
     # Save the LLVM IR to a file
     with open("./output.ll", "w+") as f:
         # print current directory
-        print("Current directory: ", os.getcwd())
-
         print("Writing LLVM IR to output.ll")
         f.write(llvm_ir)
     print(llvm_ir)
