b73e0282ecb88c6d0941eaa20de403c0c2960922
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-05-29 10:33:22 +0100

    Refactor main function to improve readability and optimize variable assignments

--- example.pl
+++ example.pl
@@ -1,35 +1,28 @@
-    #     var x : int := 120;
-    #     var y : int := 10;
-    #     var z : int := 0;
-    #     x++;
-    #     y--;
-    #     ++x;
-    #     --y;
-    #     --x;
-    #     x += 1;
-    #     while (z < 10) {
-    #         x += 2;
-    #         z++;
-    #     }
-    #     do {
-    #         x += 2;
-    #         z++;
-    #     } while (z < 20);
-    #     if (x > 10) {
-    #         x += 10;
-    #     } else {
-    #         x -= 10;
-    #     }
-    #     y -= 1;
-    #     x := x + y    + (x                                          *4);
-    #     return x + y;
     function main(): int {
-    var x : int := 120;
-    var y : int := 10;
-    if (x > 10) {
-        x += 10;
-    } else {
-        x -= 10;
-    }
-    return x;
+        var x : int := 120;
+        var y : int := 10;
+        var z : int := 0;
+        x++;
+        y--;
+        ++x;
+        --y;
+        --x;
+        x += 1;
+        while (z < 10) {
+            x += 2;
+            z++;
+        }
+        do {
+            x += 2;
+            z++;
+        } while (z < 20);
+        if (x > 10) {
+            x += 10;
+        } else {
+            x -= 10;
+        }
+        y -= 1;
+        x := x + y    + (x                                          *4);
+        return x + y;
+    
     }
