34261e98133d1595587ffdd63701c3dc79ac103d
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-05-30 01:43:07 +0100

    Refactor code to handle break and continue statements in while loops

--- example.pl
+++ example.pl
@@ -1,3 +1,7 @@
+    function aux(x: int): int {
+        return 10 * x;
+    }
+    
     function main(): int {
         var x : int := 120;
         var y : int := 10;
@@ -8,14 +12,29 @@
         --y;
         --x;
         x += 1;
+
+        var a : int := aux(y);
+        
         while (z < 10) {
             x += 2;
             z++;
         }
+
         do {
             x += 2;
             z++;
         } while (z < 20);
+
+        while (z < 100) {
+            x += 2;
+            z++;
+            if (z % 2 == 0) {
+                continue;
+            }
+            if (z == 25) {
+                break;
+            }
+        }
         if (x > 10) {
             x += 10;
         } else {
