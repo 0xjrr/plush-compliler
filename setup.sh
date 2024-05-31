6de06e0c4f04584a63e3762a8f63c24fa281a09c
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-05-31 13:32:56 +0100

    Refactor setup.sh to install LLVM and Clang along with Python dependencies

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ sudo apt update
 # Install dependencies
 sudo apt install -y python3 python3-pip
 
+# Install LLVM and Clang
+sudo apt install -y llvm clang
+
 # Install Python packages
 pip3 install ply json
 
